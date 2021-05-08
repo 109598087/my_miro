f7e56a357260d89692b6665573f518fbf1b634f4
109598087 <t109598087@ntut.org.tw>
2021-05-08 21:47:04 +0800

    add to do

--- board/src/main/java/ntut/csie/islab/miro/adapter/repository/textfigure/TextFigureRepository.java
+++ board/src/main/java/ntut/csie/islab/miro/adapter/repository/textfigure/TextFigureRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public class TextFigureRepository {
+public class TextFigureRepository {  //todo: repository test
     private List<TextFigure> textFigures;
 
     public TextFigureRepository() {
