705fcdcf804977e3394d2b84c98b9abd9d8eeb15
109598087 <t109598087@ntut.org.tw>
2021-05-08 17:04:42 +0800

    Board get test ok

---
+++ board/src/main/java/ntut/csie/islab/miro/entity/board/Board.java
@@ -0,0 +1,32 @@
+package ntut.csie.islab.miro.entity.board;
+
+import ntut.csie.sslab.ddd.model.AggregateRoot;
+
+import java.util.UUID;
+
+public class Board extends AggregateRoot<UUID> {
+    private UUID teamId;
+    private String boardName;
+
+    public Board(UUID teamId, String boardName) {
+        super(UUID.randomUUID());
+        this.teamId = teamId;
+        this.boardName = boardName;
+    }
+
+    public UUID getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(UUID teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public void setBoardName(String boardName) {
+        this.boardName = boardName;
+    }
+}
---
+++ board/src/test/java/ntut/csie/islab/miro/entity/board/BoardTest.java
@@ -0,0 +1,22 @@
+package ntut.csie.islab.miro.entity.board;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+
+    @Test
+    public void board_get_test() {
+        UUID teamId = UUID.randomUUID();
+        String boardName = "board_name";
+        Board board = new Board(teamId, boardName);
+
+        // getTeamIdTest
+        assertEquals(teamId, board.getTeamId());
+        // getBoardNameTest
+        assertEquals(boardName, board.getBoardName());
+    }
+}
