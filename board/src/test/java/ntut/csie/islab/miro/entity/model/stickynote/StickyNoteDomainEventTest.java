96a8801a968b3472818ce4c9f7b60f107c7a81db
109598087 <t109598087@ntut.org.tw>
2021-05-11 14:47:43 +0800

    my test OK

--- board/src/test/java/ntut/csie/islab/miro/entity/model/stickynote/StickyNoteDomainEventTest.java
+++ board/src/test/java/ntut/csie/islab/miro/entity/model/stickynote/StickyNoteDomainEventTest.java
@@ -4,6 +4,7 @@ import ntut.csie.islab.miro.entity.model.textFigure.TextFigure;
 import ntut.csie.islab.miro.entity.model.textFigure.Position;
 import ntut.csie.islab.miro.entity.model.textFigure.ShapeKindEnum;
 import ntut.csie.islab.miro.entity.model.textFigure.Style;
+import ntut.csie.islab.miro.entity.model.textFigure.TextFigure;
 import ntut.csie.islab.miro.entity.model.textFigure.stickynote.StickyNote;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +13,10 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StickyNoteDomainEventTest {
-    private TextFigure createStickyNote() {
-        return new StickyNote(UUID.randomUUID(), new Position(1.0, 1.0), "content", new Style(10, ShapeKindEnum.TRIANGLE, 87.2, 100, "#123456"));
-    }
 
     @Test
-    public void create_a_stickyNote_then_publishes_a_stickyNote_created_domain_event() {
-        TextFigure stickyNote = createStickyNote();
+    public void create_a_stickyNote_than_publishes_a_stickyNote_created_domain_event_test() {
+        TextFigure stickyNote = new StickyNote(UUID.randomUUID(), new Position(1.0, 1.0), "stickynote_content", new Style(20, ShapeKindEnum.RECTANGLE, 100, 100, "f9f900"));
         assertEquals(1, stickyNote.getDomainEvents().size());
     }
-
 }
