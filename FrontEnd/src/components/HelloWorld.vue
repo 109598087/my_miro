f82e0803ee90db38bad54f166095539ff784b9b1
109598087 <t109598087@ntut.org.tw>
2021-05-17 21:24:35 +0800

    fabirc canvas ok

--- FrontEnd/src/components/HelloWorld.vue
+++ FrontEnd/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <canvas id="canvas" width="1920" height="600" ref="cvs"></canvas>
     <button>createStickyNote</button>
   </div>
 <!--  template 下，只能有一個東西，不然會報錯!!!-->
@@ -8,12 +9,24 @@
 </template>
 
 <script>
+import { fabric } from 'fabric' // npm install --save fabric
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted () {
+    let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    this.canvas = new fabric.Canvas('canvas', {
+      width: width,
+      height: height,
+      fireRightClick: true, // <-- enable firing of right click events
+      stopContextMenu: true, // <--  prevent context menu from showing
+      freeDrawingCursor: 'none'
+    })
   },
   methods: {
     createStickyNote () {
